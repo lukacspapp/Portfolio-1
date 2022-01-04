dafd4699c2091fa8b33c1808836468bd8a51c603
lukacspapp <papplukacs@hotmail.com>
2022-01-04 19:53:23 +0000

    choosing color

--- Portfolio/client/src/components/contact/Contact.jsx
+++ Portfolio/client/src/components/contact/Contact.jsx
@@ -69,12 +69,13 @@ const Contact = () => {
               </svg><a className='linkedin' href='https://www.linkedin.com/in/lukacsjpapp/'>LinkedIn Profile</a>
             </div>
             </a>
+            <a href='https://goo.gl/maps/GSbFiZGgHH331WxW9'>
             <div className="c-info-item-c">
               <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
-              </svg>
-              London, E14, United Kingdom
+              </svg><a className='linkedin' href='https://goo.gl/maps/GSbFiZGgHH331WxW9'>London, E14, United Kingdom</a>
             </div>
+            </a>
           </div>
         </div>
         <div className="c-right">
--- Portfolio/client/src/components/intro/Intro.jsx
+++ Portfolio/client/src/components/intro/Intro.jsx
@@ -8,7 +8,7 @@ const Intro = () => {
       <div className='i'>
         <div className='i-left'>
           <div className="i-left-wrapper">
-            <h2 className='i-intro'>Hello, My name is</h2>
+            <h2 className='i-intro'>Hey There, My name is</h2>
             <h2 className='i-name'>Lukacs Papp</h2>
             <div className="i-title">
               <div className="i-title-wrapper">
@@ -26,6 +26,11 @@ const Intro = () => {
               I am junior full stack developer who recently graduated from General Assembely London. I am looking for a Junior Postion at wheqoifhaspmanq;fnqnavjksb
             </p>
           </div>
+          {/* <div className='slidewrapper'>
+            <h1>about</h1>
+            <h1>projects</h1>
+            <h1>contact</h1>
+          </div> */}
           <svg xmlns="http://www.w3.org/2000/svg" className="scroll" fill="none" viewBox="0 0 24 24" stroke="currentColor">
   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 13l-7 7-7-7m14-8l-7 7-7-7" />
           </svg>
