8aba8e0de739f17441a3d4486329e9cb1fe2b29b
lukacspapp <papplukacs@hotmail.com>
2022-01-05 18:42:55 +0000

    a

--- Portfolio/client/src/components/about/About.jsx
+++ Portfolio/client/src/components/about/About.jsx
@@ -1,7 +1,7 @@
 import React, { useContext} from 'react'
 import './about.css'
 import { ThemeContext } from '../../context'
-import {Link} from 'react-scroll'
+import node from '../../image/Node.js_logo.svg'
 
 
 
@@ -22,51 +22,50 @@ const About = () => {
     <div className='about' id='a-about'>
       <div className="a-left">
         <div className="a-card">
-          <a href='https://github.com/lukacspapp'>
           <div className="c-info-item">
-            <svg style={{ color: !darkMode && 'white'}} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16">
-              <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
-            </svg><a style={{ color: !darkMode && 'white'}} href='https://github.com/lukacspapp' className='ab-link'>Github Profile</a>
-          </div>
-          </a>
-          <a href='https://www.linkedin.com/in/lukacsjpapp/'>
-          <div className="c-info-item">
-            <svg style={{ color: !darkMode && 'white'}} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-linkedin" viewBox="0 0 16 16">
-              <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z" /><use xlinkHref='https://www.linkedin.com/in/lukacsjpapp' />
-            </svg><a style={{ color: !darkMode && 'white'}} className='linkedin' href='https://www.linkedin.com/in/lukacsjpapp/'>LinkedIn Profile</a>
-          </div>
-          </a>
-          <a href='mailto:papplukacs@hotmail.com'>
-          <div className="c-info-item">
-            <svg style={{ color: !darkMode && 'white'}} xmlns="http://www.w3.org/2000/svg" class="h-2 w-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
-            </svg>
-            <div style={{ color: !darkMode && 'white'}}>papplukacs@hotmail.com</div>
-          </div>
-          </a>
-          <div className="c-info-item">
-            <svg style={{ color: !darkMode && 'white'}} xmlns="http://www.w3.org/2000/svg" className='phone' class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
-            </svg>
-            <div style={{ color: !darkMode && 'white'}}>+44 (0) 7577 244977</div>
+            <img className='icon' alt='node' src='https://upload.wikimedia.org/wikipedia/commons/d/d9/Node.js_logo.svg'></img>
           </div>
           <div className="c-info-item">
-            <svg style={{ color: !darkMode && 'white'}} xmlns="http://www.w3.org/2000/svg" className='phone' class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
-            </svg>
-            <div style={{ color: !darkMode && 'white'}}>+44 (0) 7577 244977</div>
+            <img className='icon' src='https://symbols.getvecta.com/stencil_79/88_expressjs-icon.54bb6035d3.svg' alt='express'></img>
           </div>
           <div className="c-info-item">
-            <svg style={{ color: !darkMode && 'white'}} xmlns="http://www.w3.org/2000/svg" className='phone' class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
-            </svg>
-            <div style={{ color: !darkMode && 'white'}}>+44 (0) 7577 244977</div>
+            <img className='icon' alt='mdb' src='https://upload.wikimedia.org/wikipedia/commons/9/93/MongoDB_Logo.svg'></img>
           </div>
           <div className="c-info-item">
-            <svg style={{ color: !darkMode && 'white'}} xmlns="http://www.w3.org/2000/svg" className='phone' class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
-            </svg>
-            <div style={{ color: !darkMode && 'white'}}>+44 (0) 7577 244977</div>
+            <img className='python' alt='python' src='https://upload.wikimedia.org/wikipedia/commons/c/c3/Python-logo-notext.svg'></img>
+          </div>
+          <div className="c-info-item">
+            <img className='icon' alt='django' src='https://upload.wikimedia.org/wikipedia/commons/7/75/Django_logo.svg'></img>
+          </div>
+          <div className="c-info-item">
+            <img className='icon' src='https://upload.wikimedia.org/wikipedia/commons/2/29/Postgresql_elephant.svg' alt='postgres'></img>
+          </div>
+          <div className="c-info-item">
+            <img className='icon' alt='js' src='https://upload.wikimedia.org/wikipedia/commons/9/99/Unofficial_JavaScript_logo_2.svg'></img>
+          </div>
+          <div className="c-info-item">
+            <img className='icon' src='https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg' alt='react'></img>
+          </div>
+          <div className="c-info-item">
+            <img className='icon' alt='html' src='https://upload.wikimedia.org/wikipedia/commons/6/61/HTML5_logo_and_wordmark.svg'></img>
+          </div>
+          <div className="c-info-item">
+            <img className='icon' alt='css' src='https://upload.wikimedia.org/wikipedia/commons/d/d5/CSS3_logo_and_wordmark.svg'></img>
+          </div>
+          <div className="c-info-item">
+            <img className='icon' alt='bulma' src='https://cdn.worldvectorlogo.com/logos/bulma.svg'></img>
+          </div>
+          <div className="c-info-item">
+            <img className='icon' alt='bootstrap' src='https://cdn.worldvectorlogo.com/logos/bootstrap-5-1.svg'></img>
+          </div>
+          <div className="c-info-item">
+            <img className='icon' alt='api' src='https://uxwing.com/wp-content/themes/uxwing/download/07-web-app-development/rest-api.svg'></img>
+          </div>
+          <div className="c-info-item">
+            <img className='icon' alt='mongoose' src='https://camo.githubusercontent.com/7c669e872b214571ae0b5097e8d3db369225a806dc2ce9a436cde3497164310c/687474703a2f2f6d6f6e676f64622d746f6f6c732e636f6d2f696d672f6d6f6e676f6f73652e706e67'></img>
+          </div>
+          <div className="c-info-item">
+            <img className='icon' alt='postman' src='https://www.svgrepo.com/show/354202/postman-icon.svg'></img>
           </div>
         </div>
       </div>
