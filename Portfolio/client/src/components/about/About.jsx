1eb45d7103399711e60aef7d5ab35d1b7791d4fd
lukacspapp <papplukacs@hotmail.com>
2022-01-10 17:44:48 +0000

    fixing mobile view

--- Portfolio/client/src/components/about/About.jsx
+++ Portfolio/client/src/components/about/About.jsx
@@ -1,6 +1,6 @@
-import React, { useContext} from 'react'
+import React from 'react'
 import './about.css'
-import { ThemeContext } from '../../context'
+// import { ThemeContext } from '../../context'
 
 
 
@@ -11,9 +11,7 @@ const About = () => {
 
 
 
-  const theme = useContext(ThemeContext)
-  const darkMode = theme.state.darkMode
-
+  
 
 
 
@@ -22,50 +20,62 @@ const About = () => {
       <div className="a-left">
         <div className="a-card">
           <div className="c-info-item">
-            <img className='icon' alt='node' src='https://upload.wikimedia.org/wikipedia/commons/d/d9/Node.js_logo.svg'></img>
+            <i class="devicon-html5-plain icon-style"></i>
+            <p className='i-p'>HTML</p>
           </div>
           <div className="c-info-item">
-            <img className='icon' src='https://symbols.getvecta.com/stencil_79/88_expressjs-icon.54bb6035d3.svg' alt='express'></img>
+            <i class="devicon-css3-plain-wordmark"></i>
+            <p className='i-p'>CSS</p>
           </div>
           <div className="c-info-item">
-            <img className='icon-mbd' alt='mdb' src='https://upload.wikimedia.org/wikipedia/commons/9/93/MongoDB_Logo.svg'></img>
+            <i class="devicon-express-original"></i>
+            <p className='i-p'>Express</p>
           </div>
           <div className="c-info-item">
-            <img className='python' alt='python' src='https://upload.wikimedia.org/wikipedia/commons/c/c3/Python-logo-notext.svg'></img>
+            <i class="devicon-python-plain"></i>
+            <p className='i-p'>Python</p>
           </div>
           <div className="c-info-item">
-            <img className='icon-d' alt='django' src='https://upload.wikimedia.org/wikipedia/commons/7/75/Django_logo.svg'></img>
+            <i class="devicon-django-plain"></i>
+            <p className='i-p'>Django</p>
           </div>
           <div className="c-info-item">
-            <img className='icon' src='https://upload.wikimedia.org/wikipedia/commons/2/29/Postgresql_elephant.svg' alt='postgres'></img>
+            <i class="devicon-postgresql-plain"></i>
+            <p className='i-p'>PostgreSQL</p>
           </div>
           <div className="c-info-item">
-            <img className='icon' alt='js' src='https://upload.wikimedia.org/wikipedia/commons/9/99/Unofficial_JavaScript_logo_2.svg'></img>
+            <i class="devicon-javascript-plain"></i> 
+            <p className='i-p'>JavaScript</p>        
           </div>
           <div className="c-info-item">
-            <img className='icon' src='https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg' alt='react'></img>
+            <i class="devicon-react-original"></i>
+            <p className='i-p'>React</p>
           </div>
           <div className="c-info-item">
-            <img className='icon' alt='html' src='https://upload.wikimedia.org/wikipedia/commons/6/61/HTML5_logo_and_wordmark.svg'></img>
+            <i class="devicon-bulma-plain"></i>
+            <p className='i-p'>Bulma</p>
           </div>
           <div className="c-info-item">
-            <img className='icon' alt='css' src='https://upload.wikimedia.org/wikipedia/commons/d/d5/CSS3_logo_and_wordmark.svg'></img>
+            <i class="devicon-bootstrap-plain"></i>
+            <p className='i-p'>Bootstrap</p>
           </div>
           <div className="c-info-item">
-            <img className='icon' alt='bulma' src='https://cdn.worldvectorlogo.com/logos/bulma.svg'></img>
+            <i class="devicon-mongodb-plain"></i>
+            <p className='i-p'>MongoDB</p>
           </div>
           <div className="c-info-item">
-            <img className='icon' alt='bootstrap' src='https://cdn.worldvectorlogo.com/logos/bootstrap-5-1.svg'></img>
+            <i class="devicon-git-plain"></i>
+            <p className='i-p'>Git</p>
           </div>
           <div className="c-info-item">
-            <img className='icon' alt='api' src='https://uxwing.com/wp-content/themes/uxwing/download/07-web-app-development/rest-api.svg'></img>
+            <i class="devicon-yarn-plain"></i>
+            <p className='i-p'>Yarn</p>
           </div>
           <div className="c-info-item">
-            <img className='icon-m' alt='mongoose' src='https://camo.githubusercontent.com/7c669e872b214571ae0b5097e8d3db369225a806dc2ce9a436cde3497164310c/687474703a2f2f6d6f6e676f64622d746f6f6c732e636f6d2f696d672f6d6f6e676f6f73652e706e67'></img>
+            <i class="devicon-heroku-original"></i>
+            <p className='i-p'>Heroku</p>
           </div>
-          <div className="c-info-item">
-            <img className='icon' alt='postman' src='https://www.svgrepo.com/show/354202/postman-icon.svg'></img>
-          </div>
+
         </div>
       </div>
       <div className="a-right">
@@ -79,6 +89,7 @@ const About = () => {
         and I thrive when collaborating on forward-thinking and innovative projects with a passionate team of people.
         </p>
       </div>
+  
     </div>
   )
 }
--- Portfolio/client/src/components/projects/Projects.jsx
+++ Portfolio/client/src/components/projects/Projects.jsx
@@ -17,14 +17,11 @@ const Projects = () => {
         <div className="pro-texts">
           <h1 className="project-title">Projects</h1>
           <p className="project-desc">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati rerum nihil, temporibus adipisci consectetur iure blanditiis dicta. Iure sapiente dignissimos deleniti non repellendus ad dicta temporibus, vero nobis fugit aliquam.
           </p>
           <br></br>
-          <hr/>
         </div>
-        
         <div className='columns'>
-          <div className="pro-list column">CICERO<button><a className='readme' href='https://github.com/lukacspapp/SEI-Project-4-Cicero'>ReadMe</a></button>
+          <div className="pro-list">CICERO<button><a className='readme' href='https://github.com/lukacspapp/SEI-Project-4-Cicero'>ReadMe</a></button>
             <div className="pro-title">
               <p className="pro-desc">Full-stack e-commerce app built with React, Python, and Django. I created my own RESTful APIs, had authentication as part of our back-end, and also used Bootstrap. I focused more on the back-end part of the project.</p>
             </div>
@@ -32,7 +29,7 @@ const Projects = () => {
               <Cicero key={item.id} img={item.img} link={item.link}/>
             ))}
           </div>
-          <div className="pro-list column">TOKENIZER<button><a href='https://github.com/lukacspapp/SEI-Project-3-Tokenizer'>ReadMe</a></button>
+          <div className="pro-list">TOKENIZER<button><a href='https://github.com/lukacspapp/SEI-Project-3-Tokenizer'>ReadMe</a></button>
             <div className="pro-title">
               <p className="pro-desc">Built a full-stack MERN e-commerce application using our own RESTful APIs, where users can create, sell and buy their own or existing NFTs. I particularly focused on the back-end and the design.</p>
             </div>
@@ -42,7 +39,7 @@ const Projects = () => {
           </div>
         </div>
         <div className='columns'>
-        <div className="pro-list column">BREWDOG API<button><a href='https://github.com/lukacspapp/SEI-Project-2-Brewdog'>ReadMe</a></button>
+        <div className="pro-list">BREWDOG API<button><a href='https://github.com/lukacspapp/SEI-Project-2-Brewdog'>ReadMe</a></button>
             <div className="pro-title">
               <p className="pro-desc">Used React, JavaScript, CSS with Bulma and Brewdog's external public API, where we built an application to showcase Brewdog beers and compare them with a filter.</p>
             </div>
@@ -50,9 +47,9 @@ const Projects = () => {
               <Brewdog key={item.id} img={item.img} link={item.link}/>
             ))}
           </div>
-          <div className="pro-list column">SPACE INVADERS<button><a href='https://github.com/lukacspapp/SEI-Project-1-Space-Invaders'>ReadMe</a></button>
+          <div className="pro-list">SPACE INVADERS<button><a href='https://github.com/lukacspapp/SEI-Project-1-Space-Invaders'>ReadMe</a></button>
             <div className="pro-title">
-              <p className="pro-desc">I recreated a similar version of the retro Space Invaders game using HTML, CSS, and JavaScript.</p>
+              <p className="pro-desc">I recreated a browser version of the retro Space Invaders game using HTML, CSS, and JavaScript.</p>
             </div>
             {SpaceData.map((item) => (
               <Space key={item.id} img={item.img} link={item.link}/>
